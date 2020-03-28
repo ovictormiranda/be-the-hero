8266ba3ae7c222d3a3633ac922261ae536d7877d
ovictormiranda <carreira.victormiranda@gmail.com>
2020-03-28 17:34:58 -0300

    att

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -52,7 +52,7 @@ export default function Incidents() {
             <View style={styles.header}>
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
+                    Total de <Text style={styles.headerTextBold}>{total} casos.  </Text>   
                 </Text>
             </View>
 
